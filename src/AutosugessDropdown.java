42f843f0564af592f75965ff79e959e9d813471a
Home-PC <allanithomas@outlook.com>
2025-12-27 16:26:21 +0530

    from PC

--- src/AutosugessDropdown.java
+++ src/AutosugessDropdown.java
@@ -13,14 +13,14 @@ public class AutosugessDropdown {
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		
-		System.setProperty("webdriver.chrome.driver", "C:/Users/167557/Documents/chromedriver-win64/chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\allan\\OneDrive\\Documents\\chromedriver-win64\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
 		driver.findElement(By.id("checkBoxOption1")).click();
 		driver.findElement(By.id("checkBoxOption1")).click();
 		Assert.assertFalse(driver.findElement(By.id("checkBoxOption1")).isSelected());
 		System.out.println(driver.findElements(By.xpath("//input[@type='checkbox']")).size());
-		
+		driver.quit();
 
 	}
 
